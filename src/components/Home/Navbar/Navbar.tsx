e0c397d032dd2f76a747778209377b8af858f095
Mohamed Zaki <87452598+Mohamed-A-Zaki@users.noreply.github.com>
2023-08-17 21:58:36 +0300

    upload

--- src/components/Home/Navbar/Navbar.tsx
+++ src/components/Home/Navbar/Navbar.tsx
@@ -1,7 +1,132 @@
+import {
+  AppBar,
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Menu,
+  MenuItem,
+  SxProps,
+  TextField,
+  Theme,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+
+import logo from "../../../assets/logo.png";
+
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+import MenuIcon from "@mui/icons-material/Menu";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+
 export default function Navbar() {
+  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
   return (
-    <>
-      <h1>Navbar</h1>
-    </>
+    <AppBar position="static" sx={{ bgcolor: "#000", py: 1 }}>
+      <Container fixed>
+        <Toolbar disableGutters>
+          <Box component={Link} to="/">
+            <Box component={"img"} src={logo} sx={LogoStyle} />
+          </Box>
+
+          <TextField
+            type="search"
+            placeholder="ابحث..."
+            size="small"
+            sx={SearchStyle}
+          />
+
+          <Box sx={{ display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              <MenuItem onClick={handleCloseNavMenu}>
+                <Typography textAlign="center">دخول</Typography>
+              </MenuItem>
+
+              <MenuItem onClick={handleCloseNavMenu}>
+                <Typography textAlign="center">العربة</Typography>
+              </MenuItem>
+            </Menu>
+          </Box>
+
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
+            <Button onClick={handleCloseNavMenu} sx={ButtonStyle}>
+              <AccountCircleIcon />
+              دخول
+            </Button>
+
+            <Button onClick={handleCloseNavMenu} sx={ButtonStyle}>
+              <AddShoppingCartIcon />
+              العربة
+            </Button>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
+
+const ButtonStyle: SxProps<Theme> = {
+  my: 2,
+  color: "white",
+  minWidth: "fit-content",
+  display: "flex",
+  gap: 1,
+};
+
+const SearchStyle: SxProps<Theme> = {
+  flex: 1,
+  bgcolor: "#fff",
+  borderRadius: 2,
+  "& input": {
+    textAlign: "center !important",
+  },
+  "& .MuiOutlinedInput-root > fieldset": {
+    borderRadius: 2,
+  },
+};
+
+const LogoStyle: SxProps<Theme> = {
+  ml: 1,
+  width: 60,
+  height: 60,
+  objectFit: "contain",
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
