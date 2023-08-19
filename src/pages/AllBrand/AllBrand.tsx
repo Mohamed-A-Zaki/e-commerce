5c699a713f0e00b91ce4472a99326e8aef714d48
Mohamed Zaki <87452598+Mohamed-A-Zaki@users.noreply.github.com>
2023-08-19 04:20:08 +0300

    upload

--- src/pages/AllBrand/AllBrand.tsx
+++ src/pages/AllBrand/AllBrand.tsx
@@ -1,7 +1,14 @@
+import BrandList from "../../utility/BrandList/BrandList";
+import MainSection from "../../utility/MainSection/MainSection";
+import SectionHeading from "../../utility/SectionHeading/SectionHeading";
+
 export default function AllBrand() {
   return (
-    <>
-      <h1>AllBrand</h1>
-    </>
+    <MainSection>
+      <SectionHeading title={"كل الماركات"} />
+      <BrandList />
+      <BrandList />
+      <BrandList />
+    </MainSection>
   );
 }
--- src/pages/AllCategory/AllCategory.tsx
+++ src/pages/AllCategory/AllCategory.tsx
@@ -1,7 +1,14 @@
+import MainSection from "../../utility/MainSection/MainSection";
+import CategoryList from "../../utility/CategoryList/CategoryList";
+import SectionHeading from "../../utility/SectionHeading/SectionHeading";
+
 export default function AllCategory() {
   return (
-    <>
-      <h1>AllCategory</h1>
-    </>
+    <MainSection>
+      <SectionHeading title={"كل التصنيفات"} />
+      <CategoryList />
+      <CategoryList />
+      <CategoryList />
+    </MainSection>
   );
 }
