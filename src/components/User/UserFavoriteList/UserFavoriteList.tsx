60db7a6ec0b72bf56fdd25bd05defdd55e280a03
Mohamed Zaki <87452598+Mohamed-A-Zaki@users.noreply.github.com>
2023-08-26 16:49:41 +0300

    upload

--- src/components/User/UserFavoriteList/UserFavoriteList.tsx
+++ src/components/User/UserFavoriteList/UserFavoriteList.tsx
@@ -1,5 +1,5 @@
 import { Grid } from "@mui/material";
-import ProductCard from "../../../utility/Product/ProductCard/ProductCard";
+// import ProductCard from "../../../utility/Product/ProductCard/ProductCard";
 
 export default function UserFavoriteList() {
   return (
@@ -7,7 +7,7 @@ export default function UserFavoriteList() {
       {[...Array(6)].map((_, indx) => {
         return (
           <Grid item key={indx} xs={12} md={6} lg={4}>
-            <ProductCard />
+            {/* <ProductCard  /> */}
           </Grid>
         );
       })}
