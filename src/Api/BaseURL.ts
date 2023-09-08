c604b4bf2b7e08ec216d5f49ed25fd68293f5fbe
Mohamed Zaki <87452598+Mohamed-A-Zaki@users.noreply.github.com>
2023-09-08 21:06:32 +0300

    upload

--- src/Api/BaseURL.ts
+++ src/Api/BaseURL.ts
@@ -1,5 +1,8 @@
 import axios from "axios";
 
-const BaseURL = axios.create({ baseURL: "http://127.0.0.1:8000/" });
+// const BaseURL = axios.create({ baseURL: "http://127.0.0.1:8000/" });
+const BaseURL = axios.create({
+  baseURL: "https://e-commerce-backend-b89n.onrender.com/",
+});
 
 export default BaseURL;
